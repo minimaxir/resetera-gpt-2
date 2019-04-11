e3521504a2d0eed598848427b2f9aa74aed31735
Max Woolf <max@minimaxir.com>
2019-04-10 19:05:27 -0700

    Working scraper

--- resetera_scraper.py
+++ resetera_scraper.py
@@ -1,22 +1,56 @@
 import requests
 import time
+from jinja2 import Environment, select_autoescape
 from tqdm import trange
 from bs4 import BeautifulSoup
 
-BASE_URL = "https://www.resetera.com/forums/video-games.7/page-{}"
+DOMAIN = "https://www.resetera.com"
+BASE_URL = DOMAIN + "/forums/video-games.7/page-{}"
 HEADERS = {
     'User-Agent': 'ResetERA user minimaxir',
 }
+MAX_PAGES = 2
 
-max_pages = 1164
+env = Environment()
+
+template_str = """~~~{{ title }}~~~
+
+{% for post in posts[:20] -%}
+{{ post['data-author'] }}: {{ post.div.article.text | trim }}
+-----
+{% endfor %}
+!~END~!
+"""
+
+template = env.from_string(template_str)
+
+
+def process_thread(thread_url, template):
+    req = requests.get(thread_url, headers=HEADERS)
+    soup = BeautifulSoup(req.text, features="html5lib")
+
+    # Remove special embeds
+    bbcodes = soup.find_all("div", {"class": "bbCodeBlock"})
+    for bbcode in bbcodes:
+        bbcode.decompose()
+
+    title = soup.find("h1", {"class": "p-title-value"}).text
+    if 'OT' in title:
+        return None
+    posts = soup.find_all("article", {"class": "message"})
+
+    text = template.render(title=title, posts=posts)
+    return text
 
 with open('resetera_videogames.txt', 'w') as f:
-    for page in trange(1, max_pages):
+    for page in trange(1, MAX_PAGES+1):
         url = BASE_URL.format(page)
         req = requests.get(url, headers=HEADERS)
         soup = BeautifulSoup(req.text, features="html5lib")
-        titles = [x.a.text for x in soup.find_all(
+        thread_urls = [DOMAIN + x.a['href'] for x in soup.find_all(
             "div", {"class": "structItem-title"})]
-        for title in titles:
-            f.write(title + "\n")
+        for thread_url in thread_urls:
+            thread = process_thread(thread_url, template)
+            if thread is not None:
+                f.write(thread + "\n")
         time.sleep(1)  # to avoid overloading server
